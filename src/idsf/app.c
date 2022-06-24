b902edee0c5fa30b96bdd10f02437ca4c65b3389
NLag <NLag@users.noreply.github.com>
2022-06-24 14:34:59 +0200

    create  function app for IDS

---
+++ src/idsf/app.c
@@ -0,0 +1,20 @@
+#include "ogs-app.h"
+
+int app_initialize(const char *const argv[])
+{
+    int rv;
+
+    rv = idsf_initialize();
+    if (rv != OGS_OK) {
+        ogs_error("Failed to intialize IDSF");
+        return rv;
+    }
+    ogs_info("IDSF initialize...done");
+
+    return OGS_OK;
+}
+
+void app_terminate(void)
+{
+    idsf_terminate();
+    ogs_info("IDSF terminate...done");
