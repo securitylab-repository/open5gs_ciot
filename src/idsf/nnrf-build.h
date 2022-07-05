24cf4f5ea42e562b44711a2b4090435915657f46
NLag <NLag@users.noreply.github.com>
2022-07-05 17:06:11 +0200

    add nnrf-build.h and nnrf-build.c for idsf

---
+++ src/idsf/nnrf-build.h
@@ -0,0 +1,21 @@
+/* 
+    created by le linh
+    based on similar component of pcf,ausf
+*/
+
+#ifndef IDSF_NNRF_BUILD_H
+#define IDSF_NNRF_BUILD_H
+
+#include "context.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+ogs_sbi_request_t *idsf_nnrf_nfm_build_register(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* IDSF_NNRF_BUILD_H */
