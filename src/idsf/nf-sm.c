d18df67088cb95e516bf8656f1c9f247936aecdd
NLag <NLag@users.noreply.github.com>
2022-07-13 14:10:48 +0200

    Revert "add info check NRF instance"
    This reverts commit 073d0c09114151d84c3503ff2769e269b3d55360.
    
    Edited wrong file, revert ausf to original

--- src/idsf/nf-sm.c
+++ src/idsf/nf-sm.c
@@ -57,8 +57,13 @@ void idsf_nf_state_initial(ogs_fsm_t *s, idsf_event_t *e)
     nf_instance->t_no_heartbeat->cb = idsf_timer_nf_instance_no_heartbeat;
     ogs_assert(nf_instance->t_validity);
     nf_instance->t_validity->cb = idsf_timer_nf_instance_validity;
-    
+
     ogs_info("IDSF_EVT_SBI_CLIENT --> %d", nf_instance->nf_type);
+    
+    ogs_info("[%s] is NRF ? [%d]",
+                    ogs_sbi_self()->nf_instance->id,
+                    NF_INSTANCE_IS_NRF(nf_instance));
+                    
     if (NF_INSTANCE_IS_NRF(nf_instance)) {
         OGS_FSM_TRAN(s, &idsf_nf_state_will_register);
     } else {
