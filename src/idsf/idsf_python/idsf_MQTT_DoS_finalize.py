9b540a2bf04e9b756b298dcd0bafea607062b415
NLag <NLag@users.noreply.github.com>
2023-01-24 07:03:33 +0100

    add multiple model to test

--- src/idsf/idsf_python/idsf_MQTT_DoS_finalize.py
+++ src/idsf/idsf_python/idsf_MQTT_DoS_finalize.py
@@ -3,9 +3,6 @@ from scapy.all import *
 from scapy.all import ls,raw,load_layer
 from scapy.contrib.gtp import GTPHeader
 from scapy.layers.inet import IP, TCP, UDP
-from scapy.layers.http import HTTP, HTTPRequest, HTTPResponse
-from scapy.layers.dns import DNS,DNSQR
-from scapy.layers.dhcp import DHCP
 from scapy.layers.tls.record import TLS
 from scapy.layers.tls.handshake import TLSServerHello
 from scapy.contrib.mqtt import MQTT
@@ -19,31 +16,19 @@ load_layer('tls')
 load_layer('dhcp')
 
 # config
-maxMessageSize = 1500
+maxMessageSize = 65535
 UDP_IP = "127.0.0.25"
 UDP_PORT = 2152
 
 # bind socket
 sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
+                     socket.SOCK_DGRAM,
+                     socket.IPPROTO_UDP) # UDP
 sock.bind((UDP_IP, UDP_PORT))
 
 print("binding to ip %s port %d" % (UDP_IP, UDP_PORT))
 
 ################################################################
-# load IP dict
-
-stat_file = 'stat_data.json' 
-report_file = 'report.stat'
-
-import json
-import os.path
-
-if os.path.exists(stat_file):
-    with open(stat_file,'r') as file:
-        stat_dict = json.load(file)
-else:
-    stat_dict = {}
 
 #load feature names
 featurenamefile = 'Feature_name.dat'
@@ -57,41 +42,65 @@ import pandas as pd
 import joblib
 
 modelfile = 'IDSF_model_'
-modeltype = 'Neural'
+modeltypes = ['DT','LSVC','SGDC','RBF_SVC_auto10','RBF_SVC_scale1','RBF_SVC_scale10','Neural']
 modelext = '.joblib'
-filename = modelfile + modeltype + modelext
-loaded_model = joblib.load(filename)
+model_dict = {}
+for modeltype in modeltypes:
+    filename = modelfile + modeltype + modelext
+    model_dict['modeltype'] = joblib.load(filename)
+
+################################################################
+
+# handle statistic 
+
+main_stats = {}
+main_stats['start_time'] = time.strftime("%d-%m-%Y %H:%M:%S %z")
+main_stats['packet_count'] = 0
+
+for modeltype in modeltypes:
+    model_stats = { 'TP': 0,
+                    'FP': 0,
+                    'TN': 0,
+                    'FN': 0,
+                    'accuracy': 0,
+                    'specificity': 0,
+                    'sensitivity': 0,
+                    'precision': 0,
+                    'F_score': 0}
+    main_stats[modeltype] = model_stats
+
+
+report_file = 'report.dat'
 
 # before exit handle report
 
 import atexit
+import signal
+import json
 
 def exit_handler():
-    with open(stat_file, "w") as file:
-        json.dump(stat_dict,file)
-    strtime = time.strftime("%d-%m-%Y %H:%M:%S %z") + "\n"
-    strpkc = "packet count: " + str(pk_count) + "\n"
-    strtp = "true pos:  " + str(tp) + "\n"
-    strfp = "false pos: " + str(fp) + "\n"
-    strtn = "true neg:  " + str(tn) + "\n"
-    strfn = "false neg: " + str(fn) + "\n"
-    stracc = "accuracy: " + str(acc)+ "\n"
+    main_stats['end_time'] = time.strftime("%d-%m-%Y %H:%M:%S %z")
+    main_stats['packet_count'] = pk_count
+    if label != None:
+        for modeltype in modeltypes:
+            main_stats[modeltype]['accuracy'] = (main_stats[modeltype]['TP'] + main_stats[modeltype]['TN'])/main_stats['packet_count']
+            main_stats[modeltype]['specificity'] = main_stats[modeltype]['TN']/(main_stats[modeltype]['TN']+main_stats[modeltype]['FP'])
+            main_stats[modeltype]['sensitivity'] = main_stats[modeltype]['TP']/(main_stats[modeltype]['TP']+main_stats[modeltype]['FN'])
+            main_stats[modeltype]['precision'] = main_stats[modeltype]['TP']/(main_stats[modeltype]['TP']+main_stats[modeltype]['FP'])
+            main_stats[modeltype]['F_score'] = (2*main_stats[modeltype]['precision']*main_stats[modeltype]['sensitivity'])/(main_stats[modeltype]['precision']+main_stats[modeltype]['sensitivity'])
+    stat_json_str = json.dumps(main_stats) + '\n\n'
     with open(report_file,"a") as file:
-        file.write(strtime)
-        file.write(strpkc)
-        file.write(strtp)
-        file.write(strfp)
-        file.write(strtn)
-        file.write(strfn)
-        file.write(stracc)
+        file.write(stat_json_str)
     print('Dumped stat before exit')
 
 atexit.register(exit_handler)
+signal.signal(signal.SIGTERM, exit_handler)
+signal.signal(signal.SIGINT, exit_handler)
 
 ################################################################
 
 def ip_packet_to_dataframe(pk,feature_name,time_delta,time_rela):
-    df = pd.DataFrame(0,index=np.arange(1), columns=feature_name)
+    df = pd.DataFrame(0,index=[0], columns=feature_name)
     df['frame.time_delta'] = time_delta
     df['frame.time_relative'] = time_rela
     df['frame.len'] = pk[IP].len
@@ -100,78 +109,98 @@ def ip_packet_to_dataframe(pk,feature_name,time_delta,time_rela):
         df['tcp.dstport'] = pk[TCP].dport 
     
     if pk.haslayer(MQTT):
-        hdrflags = raw(pk[MQTT])
+        mqtt_pk = pk[MQTT]
+        hdrflags = raw(mqtt_pk)
         df['mqtt.hdrflags'] = hdrflags[0]
-        df['mqtt.msgtype'] = pk[MQTT].type
-        df['mqtt.dupflag'] = pk[MQTT].DUP
-        df['mqtt.qos'] = pk[MQTT].QOS
-        df['mqtt.retain'] = pk[MQTT].RETAIN
-        df['mqtt.len'] = pk[MQTT].len
+        df['mqtt.msgtype'] = mqtt_pk.type
+        df['mqtt.dupflag'] = mqtt_pk.DUP
+        df['mqtt.qos'] = mqtt_pk.QOS
+        df['mqtt.retain'] = mqtt_pk.RETAIN
+        df['mqtt.len'] = 0 if mqtt_pk.len == None else mqtt_pk.len
 
-    if pk.haslayer(MQTTConnect):
-        # conflags = raw(pk[MQTTConnect])
-        # conflags_pos = 2 + pk[MQTTConnect].length + 1
-        # df['mqtt.conflags'] = conflags[conflags_pos]
-        df['mqtt.proto_len'] = pk[MQTTConnect].length
-        if pk[MQTTConnect].protoname != '':
-            df['mqtt.protoname'] = 1 if pk[MQTTConnect].protoname == "MQTT" else 0.5
-        else:
-            df['mqtt.protoname'] = 0
-        df['mqtt.ver'] = pk[MQTTConnect].protolevel
-        
-        df['mqtt.conflag.uname'] = pk[MQTTConnect].usernameflag
-        if pk[MQTTConnect].usernameflag:
-            df['mqtt.username_len'] = pk[MQTTConnect].userlen
-        
-        df['mqtt.conflag.passwd'] = pk[MQTTConnect].passwordflag
-        if pk[MQTTConnect].passwordflag:
-            df['mqtt.passwd_len'] = pk[MQTTConnect].passlen
+        if mqtt_pk.len != 0 and mqtt_pk.haslayer(MQTTConnect):
+            mqtt_con_pk = mqtt_pk[MQTTConnect]
+            df['mqtt.proto_len'] = mqtt_con_pk.length
+            if mqtt_con_pk.protoname != '':
+                df['mqtt.protoname'] = 1 if mqtt_con_pk.protoname == "MQTT" else 0
+            else:
+                df['mqtt.protoname'] = 0
+            df['mqtt.ver'] = mqtt_con_pk.protolevel
+            
+            df['mqtt.conflag.uname'] = mqtt_con_pk.usernameflag
+            if mqtt_con_pk.usernameflag:
+                df['mqtt.username_len'] = mqtt_con_pk.userlen
+            
+            df['mqtt.conflag.passwd'] = mqtt_con_pk.passwordflag
+            if mqtt_con_pk.passwordflag:
+                df['mqtt.passwd_len'] = mqtt_con_pk.passlen
 
-        df['mqtt.conflag.willflag'] = pk[MQTTConnect].willflag
-        if pk[MQTTConnect].willflag:
-            df['mqtt.willtopic_len'] = pk[MQTTConnect].wtoplen
-            df['mqtt.willmsg_len'] = pk[MQTTConnect].wmsglen
+            df['mqtt.conflag.willflag'] = mqtt_con_pk.willflag
+            if mqtt_con_pk.willflag:
+                df['mqtt.willtopic_len'] = mqtt_con_pk.wtoplen
+                df['mqtt.willmsg_len'] = mqtt_con_pk.wmsglen
 
-        df['mqtt.conflag.retain'] = pk[MQTTConnect].willretainflag
-        df['mqtt.conflag.qos'] = pk[MQTTConnect].willQOSflag
-        
-        df['mqtt.conflag.cleansess'] = pk[MQTTConnect].cleansess
-        df['mqtt.conflag.reserved'] = pk[MQTTConnect].reserved
-        df['mqtt.kalive'] = pk[MQTTConnect].klive
-        df['mqtt.clientid_len'] = pk[MQTTConnect].clientIdlen
-        
-        
-    if pk.haslayer(MQTTConnack):
-        df['mqtt.conack.flags'] = pk[MQTTConnack].sessPresentFlag
-        df['mqtt.conack.val'] = pk[MQTTConnack].retcode
-        df['mqtt.conack.flags.reserved'] = 1 if pk[MQTTConnack].retcode >= 6 else 0
-        df['mqtt.conack.flags.sp'] = pk[MQTTConnack].sessPresentFlag % 2
+            df['mqtt.conflag.retain'] = mqtt_con_pk.willretainflag
+            df['mqtt.conflag.qos'] = mqtt_con_pk.willQOSflag
+            
+            df['mqtt.conflag.cleansess'] = mqtt_con_pk.cleansess
+            df['mqtt.conflag.reserved'] = mqtt_con_pk.reserved
+            df['mqtt.kalive'] = mqtt_con_pk.klive
+            df['mqtt.clientid_len'] = len(mqtt_con_pk.clientId)      
+            
+            conflags = mqtt_con_pk.cleansess*2 + mqtt_con_pk.willflag*4 + mqtt_con_pk.willQOSflag*8 \
+                        + mqtt_con_pk.willretainflag*32 + mqtt_con_pk.passwordflag*64 + mqtt_con_pk.usernameflag*128
+            df['mqtt.conflags'] = conflags
 
-    if pk.haslayer(MQTTPublish):
-        df['mqtt.topic_len'] = pk[MQTTPublish].length
-    
-    if pk.haslayer(MQTTSubscribe):
-        if pk.haslayer(MQTTTopic):
-            df['mqtt.sub.qos'] = pk[MQTTTopic].QOS
-            df['mqtt.topic_len'] = pk[MQTTTopic].length
+        if pk.haslayer(MQTTConnack):
+            df['mqtt.conack.flags'] = pk[MQTTConnack].sessPresentFlag
+            df['mqtt.conack.val'] = pk[MQTTConnack].retcode
+            df['mqtt.conack.flags.reserved'] = 1 if pk[MQTTConnack].retcode >= 6 else 0
+            df['mqtt.conack.flags.sp'] = pk[MQTTConnack].sessPresentFlag % 2
 
-    if pk.haslayer(MQTTSuback):
-        df['mqtt.suback.qos'] = pk[MQTTSuback].retcode
+        if pk.haslayer(MQTTPublish):
+            df['mqtt.topic_len'] = pk[MQTTPublish].length
+        
+        if pk.haslayer(MQTTSubscribe):
+            if pk.haslayer(MQTTTopic):
+                df['mqtt.sub.qos'] = pk[MQTTTopic].QOS
+                df['mqtt.topic_len'] = pk[MQTTTopic].length
+
+        if pk.haslayer(MQTTSuback):
+            df['mqtt.suback.qos'] = pk[MQTTSuback].retcode
 
     # print(df)
     return df
 
 ################################################################
 
-def AImodel_Detect_Abnormal(df,model):
-    preds = model.predict(df)
-    return preds
+from collections import Counter
+
+def AImodel_Detect_Abnormal(df,label,model_dict):
+    global main_stats
+    preds = []
+    for modeltype, model in model_dict:
+        pred = model.predict(df)
+        preds.append(pred)
+        if label != None:
+            if pred != 'normal':
+                if pred == label:
+                    main_stats[modeltype]['TP'] += 1
+                else:
+                    main_stats[modeltype]['FP'] += 1
+            else:
+                if pred == label:
+                    main_stats[modeltype]['TN'] += 1
+                else:
+                    main_stats[modeltype]['FN'] += 1
+    preds = Counter(preds)
+    final_pred,count = preds.most_common()[0]
+    return final_pred
+
+################################################################
 
 import pycurl
 from io import StringIO,BytesIO 
-import json
-
-################################################################
 
 def idsf_nsmf_send_session_release(ss_context_id):
     buffer = BytesIO()
@@ -217,20 +246,18 @@ server_ip = '10.45.0.2'
 legit_ip = '10.45.0.3'
 atk_ip = '10.45.0.4'
 
-label = False
-tp = 0
-tn = 0
-fp = 0
-fn = 0
-acc = 0
+label = None
 
 checkpoint = [1000,2000,4000,8000,16000,32000,64000,128000,256000,512000]
 
 start_time = time.time()
 pk_count = 0
+
+################################################################
+
 # release = True
 while True:
-    data, addr = sock.recvfrom(maxMessageSize) # buffer size is 65507 bytes
+    data, addr = sock.recvfrom(maxMessageSize) 
     # print(pk_count,"received",len(data)," bytes")
     
      # extract IP packet
@@ -250,38 +277,24 @@ while True:
     pkduration = now - lastpk_time
     lastpk_time = now 
 
+    # get label
     ip_packet = gtp_packet[IP]
-
     ip_src = ip_packet.src
-    label = True if ip_src == atk_ip else False
+    label = 'DoS' if ip_src == atk_ip else 'normal'
 
     try:
         df_packet = ip_packet_to_dataframe(ip_packet,ftnames,pkduration,pk_relative_time)
-        res = AImodel_Detect_Abnormal(df_packet,loaded_model)
+        res = AImodel_Detect_Abnormal(df_packet,label, model_dict)
     except Exception as e:
         ip_packet.show()
-        print(df_packet)
+        print(df_packet.shape)
+        for col in df_packet.columns:
+            print(col,' ', df_packet[col])
         traceback.print_exc()
         break
-    # print(res)
-
-    if res[0] != 'normal':
-        if label == True:
-            tp+=1
-        else:
-            fp+=1
-    else:
-        if label == False:
-            tn+=1
-        else:
-            fn+=1
-    
-    acc = (tp+tn)/pk_count
     
     if (pk_count in checkpoint) or (pk_count % 1000 ==0):
-        print("checkpoint")
-        print(pk_count,acc)
-        print(tp,fp,tn,fn)
-    
+        strcnt = 'Checkpoint(' + time.strftime("%d-%m-%Y %H:%M:%S %z") + '):' + str(pk_count) + '\n'
+        print(strcnt)
 
 ################################################################
